4fd9fc61b29ee5369a52ff19b8b4f054cd49a23f
Sungjoon Lee <sungjoon25@yonsei.ac.kr>
2025-06-10 19:32:03 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 896 ms, Memory: 42928 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -0,0 +1,83 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class Deque {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        this.size = 0;
+        this.start = null;
+        this.end = null;
+    }
+
+    append(val) {
+        const node = new Node(val)
+
+        if (this.size === 0) {
+            this.start = node;
+            this.end = node;
+        } else {
+            const cachedEnd = this.end;
+            cachedEnd.next = node;
+            node.prev = cachedEnd;
+            this.end = node;
+        }
+        
+        this.size++;
+    }
+
+    popLeft() {
+        if (this.size === 0) return null;
+
+        const val = this.start.val;
+
+        if (this.size === 1) {
+            this.init();
+        } else {
+            this.start = this.start.next;
+            this.start.prev = null;
+            this.size--;
+        }
+        
+        return val;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+const fs = require('fs');
+const [N, K] = fs.readFileSync('dev/stdin').toString().trim().split(' ').map(Number);
+
+const queue = new Deque();
+
+Array.from({length: N}, (_, i) => i + 1).forEach((p) => {
+    queue.append(p);
+})
+
+const res = [];
+let c = 1;
+
+while (!queue.isEmpty()) {
+    const person = queue.popLeft();
+
+    if (c % K !== 0) {
+        queue.append(person);
+        c++;
+    } else {
+        res.push(person);
+        c = 1;
+    }
+}
+
+console.log("<" + res.join(', ') + ">");
