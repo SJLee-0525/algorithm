33425b1342b6a2250112770ad17e3d008fb4345b
Sungjoon Lee <sungjoon25@yonsei.ac.kr>
2025-07-01 15:02:31 +0900

    [Silver III] Title: 주유소, Time: 284 ms, Memory: 46300 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -2,26 +2,18 @@ const solution = (input) => {
     const N = Number(input[0]);
 
     const dists = [...input[1].split(' ').map(BigInt), 0n];
-    for (let n = 0; n < N - 2; n++) dists[n + 1] += dists[n];
-
     const prices = input[2].split(' ').map(BigInt);
-    const temp = dists.map((dist) => dist * prices[0]);    
 
-    for (let i = 1; i < N; i++) {
-        const tempCost = temp[i - 1] + (dists[i] - dists[i - 1]) * prices[i];
+    let totalCost = 0n;
+    let minPrice = prices[0];
 
-        if (temp[i] > tempCost) {
-            change(dists, temp, i, prices[i], N);
-        }
+    for (let i = 0; i < N - 1; i++) {
+        totalCost += minPrice * dists[i];
+
+        if (minPrice > prices[i + 1]) minPrice = prices[i + 1];
     }
 
-    console.log(temp[N - 2].toString());
-}
-
-const change = (dists, temp, s, c, N) => {
-    for (let i = s; i < N - 1; i++) {
-        temp[i] = temp[s - 1] + (dists[i] - dists[s - 1]) * c;
-    }
+    console.log(totalCost.toString());
 }
 
 const fs = require('fs');
