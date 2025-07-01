d125422d3ff9ca30bf06c8d25b3bf932e330e9c0
Sungjoon Lee <sungjoon25@yonsei.ac.kr>
2025-07-01 14:52:55 +0900

    [Silver III] Title: 주유소, Time: 304 ms, Memory: 47900 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -2,19 +2,26 @@ const solution = (input) => {
     const N = Number(input[0]);
 
     const dists = [...input[1].split(' ').map(BigInt), 0n];
+    for (let n = 0; n < N - 2; n++) dists[n + 1] += dists[n];
+
     const prices = input[2].split(' ').map(BigInt);
-    
-    for (let i = N - 2; i > 0; i--) dists[i - 1] += dists[i];
+    const temp = dists.map((dist) => dist * prices[0]);    
 
-    let temp = dists.map((elem, i) => elem * prices[i]);
+    for (let i = 1; i < N; i++) {
+        const tempCost = temp[i - 1] + (dists[i] - dists[i - 1]) * prices[i];
 
-    for (let c = N - 2; c > -1; c--) {
-        const tempCost = temp[c + 1] + (dists[c] - dists[c + 1]) * prices[c];
-
-        if (temp[c] > tempCost) temp[c] = tempCost;
+        if (temp[i] > tempCost) {
+            change(dists, temp, i, prices[i], N);
+        }
     }
 
-    console.log(temp[0].toString());
+    console.log(temp[N - 2].toString());
+}
+
+const change = (dists, temp, s, c, N) => {
+    for (let i = s; i < N - 1; i++) {
+        temp[i] = temp[s - 1] + (dists[i] - dists[s - 1]) * c;
+    }
 }
 
 const fs = require('fs');
