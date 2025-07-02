e8ef40d36c8dd25f7b46fbe51be6627b3c7c7743
Sungjoon Lee <sungjoon25@yonsei.ac.kr>
2025-07-02 13:17:14 +0900

    [Silver III] Title: 특별상이라도 받고 싶어, Time: 492 ms, Memory: 72652 KB -BaekjoonHub

--- 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.js
+++ 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.js
@@ -3,26 +3,27 @@ const solution = (input) => {
 
     const arr = input.slice(1, N + 1).map((a) => a.split(' ').map(Number));
 
+    const f = (si, ei, sj, ej) => {
+        if (si > ei || sj > ej) return Infinity;
+    
+        if (si === ei && sj === ej) return arr[si][sj];
+    
+        const mi = Math.floor((si + ei) / 2);
+        const mj = Math.floor((sj + ej) / 2);
+    
+        return [
+            f(si, mi, sj, mj), 
+            f(mi + 1, ei, sj, mj), 
+            f(si, mi, mj + 1, ej), 
+            f(mi + 1, ei, mj + 1, ej)
+        ].sort((a, b) => a - b)[1];
+    }
+
     console.log(
-        f(arr, 0, N - 1, 0, N - 1)
+        f(0, N - 1, 0, N - 1)
     );
 }
 
-const f = (arr, si, ei, sj, ej) => {
-    if (si > ei || sj > ej) return Infinity;
-
-    if (si === ei && sj === ej) return arr[si][sj];
-
-    const mi = Math.floor((si + ei) / 2);
-    const mj = Math.floor((sj + ej) / 2);
-
-    return [
-        f(arr, si, mi, sj, mj), 
-        f(arr, mi + 1, ei, sj, mj), 
-        f(arr, si, mi, mj + 1, ej), 
-        f(arr, mi + 1, ei, mj + 1, ej)
-    ].sort((a, b) => a - b)[1];
-}
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
