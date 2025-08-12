27eaccd67ab825a4e896af4cf24938c069b72f9f
Sungjoon Lee <sungjoon25@yonsei.ac.kr>
2025-08-12 16:23:25 +0900

    [Gold V] Title: 영정이의 청소, Time: 268 ms, Memory: 25040 KB -BaekjoonHub

--- 백준/Gold/14671. 영정이의 청소/영정이의 청소.js
+++ 백준/Gold/14671. 영정이의 청소/영정이의 청소.js
@@ -2,23 +2,32 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const solution = (input) => {
-    const [N, M, K] = input[0].split(' ').map(Number);
+    const [_, __, K] = input[0].split(' ').map(Number);
 
+    let cnt = 0;
     const pos = Array(4).fill(false);
 
     for (let k = 1; k < K + 1; k++) {
         const [x, y] = input[k].split(' ').map(Number);
 
-        if (x % 2 === 0 && y % 2 === 0) pos[0] = true;
-        else if (x % 2 === 1 && y % 2 === 0) pos[1] = true;
-        else if (x % 2 === 0 && y % 2 === 1) pos[2] = true;
-        else pos[3] = true;
+        if (!pos[0] && x % 2 === 0 && y % 2 === 0) {
+            pos[0] = true;
+            cnt++;
+        } else if (!pos[1] && x % 2 === 1 && y % 2 === 0) {
+            pos[1] = true;
+            cnt++;
+        } else if (!pos[2] && x % 2 === 0 && y % 2 === 1) {
+            pos[2] = true
+            cnt++
+        } else if (!pos[3] && x % 2 === 1 && y % 2 === 1) {
+            pos[3] = true;
+            cnt++;
+        }
+
+        if (cnt === 4) return 'YES';
     }
 
-    for (const p of pos) if (!p) return 'NO';
-
-    return 'YES';
+    return 'NO';
 }
-
 
 console.log( solution(input) );
