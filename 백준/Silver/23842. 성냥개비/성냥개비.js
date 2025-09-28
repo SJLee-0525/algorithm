e8af5a6bdbf41a2819fe081dff7c168ed60d5842
Sungjoon Lee <sungjoon25@yonsei.ac.kr>
2025-09-28 22:06:30 +0900

    [Silver IV] Title: 성냥개비, Time: 92 ms, Memory: 9484 KB -BaekjoonHub

---
+++ 백준/Silver/23842. 성냥개비/성냥개비.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const N = Number(fs.readFileSync('/dev/stdin').toString().trim());
+
+const solution = (N) => {
+    if (N < 16) return 'impossible';
+
+    const STD = N - 4;
+    const info = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6];
+
+    for (let left = 0; left < 100; left++) {
+        const leftCnt = info[Math.floor(left / 10)] + info[left % 10];
+
+        for (let right = 0; right < 100 - left; right++) {
+            const rightCnt = info[Math.floor(right / 10)] + info[right % 10];
+            const calCnt = info[Math.floor((left + right) / 10)] + info[(left + right) % 10];
+
+            if (leftCnt + rightCnt + calCnt === STD) {
+                return `${left.toString().padStart(2, '0')}+${right.toString().padStart(2, '0')}=${(left + right).toString().padStart(2, '0')}`;
+            };
+        };
+    };
+
+    return 'impossible';
+};
+
+console.log( solution(N) );
