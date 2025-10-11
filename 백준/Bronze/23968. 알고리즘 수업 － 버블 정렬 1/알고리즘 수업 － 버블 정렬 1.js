ab663e8b807fe2766517515fa5740edf37b8ac76
Sungjoon Lee <sungjoon25@yonsei.ac.kr>
2025-10-11 19:32:10 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 428 ms, Memory: 12780 KB -BaekjoonHub

---
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const solution = (input) => {
+    const [N, K] = input[0].trim().split(' ').map(Number);
+    const arr = input[1].trim().split(' ').map(Number);
+
+    let changeCnt = 0;
+    let res = '-1';
+
+    for (let l = N - 1; l > 0; l--) {
+        for (let i = 0; i < l; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                changeCnt++;
+
+                if (changeCnt === K) {
+                    res = `${arr[i]} ${arr[i + 1]}`;
+                    break;
+                };
+            };
+        };
+    };
+
+    console.log(res);
+};
+
+solution(input);
