4ce8c8b07cc7b500c3a90d098b487ee962e6b424
Sungjoon Lee <sungjoon25@yonsei.ac.kr>
2025-10-11 18:57:33 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 224 ms, Memory: 10580 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const solution = (input) => {
+    const [N, K] = input[0].trim().split(' ').map(Number);
+    const arr = input[1].trim().split(' ').map(Number);
+
+    let changeCnt = 0;
+    let res = '-1';
+
+    for (let t = N - 1; t > 0; t--) {
+        let biggestIdx = null, biggestVal = -1;
+
+        for (let c = 0; c < t; c++) {
+            if (arr[c] > biggestVal) {
+                biggestVal = arr[c];
+                biggestIdx = c;
+            };
+        };
+
+        if (biggestIdx !== null && biggestVal > arr[t]) {
+            [arr[t], arr[biggestIdx]] = [arr[biggestIdx], arr[t]];
+            changeCnt++;
+
+            if (changeCnt === K) {
+                res = `${arr[biggestIdx]} ${arr[t]}`;
+                break;
+            };
+        };
+    };
+
+    console.log(res);
+};
+
+solution(input);
